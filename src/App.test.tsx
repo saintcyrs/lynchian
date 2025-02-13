f7bdf6b998f8b9b100af262bbc81a9ffa9820cbd
Soleil Saint-Cyr <ssaintcyr@college.harvard.edu>
2025-02-12 22:01:58 -0500

    Updates

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-import "@testing-library/jest-dom/extend-expect";
-
-test("renders the header text", () => {
-  render(<App />);
-  const headerElement = screen.getByText(/David Lynch Daily Video/i);
-});
